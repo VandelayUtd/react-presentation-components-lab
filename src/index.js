cf2e4061958b086cd7b5cecdbd0952af39b8f68e
VandelayUtd <drewhadley.guitar@gmail.com>
2021-11-20 10:45:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const clicked = "i've been clicked"
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=> alert("clicked")} />
   </div>,
   document.getElementById('root')
 );
